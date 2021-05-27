3bb811c6f2b571b19c6f1ee81262aa859f854835
unknown <nimisha@wisdom.com>
2021-05-27 10:00:04 +0530

    remaining weather app

--- src/components/WeatherApp/index.js
+++ src/components/WeatherApp/index.js
@@ -1,38 +1,52 @@
 import "./css/style.css";
-import {useState} from "react";
+import {useState, useEffect} from "react";
 
 const WeatherApp = () => {   
 
     const [city, setCity] = useState(null);
     const [search, setSearch] = useState("Mumbai");
 
-    
+    useEffect( () => {
+        const fetchApi = async () => {
+            const url = `http://api.openweathermap.org/data/2.5/weather?q=${search}&units=metric&appid=4ec34044ffd607be03899eb8b6e0dbbd`
+            const response = await fetch(url);            
+            const resJson = await response.json();
+            console.log(resJson.main);
+            setCity(resJson.main);
+        }
+
+        fetchApi();
+    },[search]);  
     
     return(
         <>
             <div className="box">
-                <div className="search-wrapper">
-                    <input type="search"   
-                    name="Search"                 
-                    id="Search"
-                    className=""
-                    onChange={(event) => { setCity(event.target.value) }
-                    }
-                    />
-                    
-                </div>
-          
 
-            <div className="info">
-                    <h2 className="location"></h2> 
-                    <i class="fas fa-street-view fa-5x"></i>{city}
-                    <h2 className="temp">
-                    5.25 Cel
-                    </h2>
-                    <h3 className="tempmin_max">Min : 5.25 Cel | Max: 5.25 cel</h3>
+            <div className="search-box">
+                            <input 
+                                type="text"
+                                className="search-bar"
+                                value={search}
+                                placeholder="Search..."
+                                onChange={e => setSearch(e.target.value)}           
+                                
+                            />
+               
             </div>
 
-          
+                {!city ? (<p>No Data Found</p>) : (
+                    <div>
+                    <div className="location-box">
+                    <div className="location"></div>
+
+                    <i className="fas fa-street-view fa-5x"></i> <span className="locationfont">{search}</span>
+                            <h2 className="temp">
+                            {city.temp}°Cel
+                            </h2>
+                            <h3 className="tempmin_max">Min : {city.temp_min}°Cel | Max: {city.temp_max}°Cel </h3>                    
+                    </div>
+                    </div>
+                )}              
 
             </div>
         </>
